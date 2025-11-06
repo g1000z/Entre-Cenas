6506cbeae14a9f7cd6bc60d203248dea521191f1
g1000z <guilherme.cavalcanti.garcia.2007@gmail.com>
2025-11-06 02:00:00 +0000

    Corrige caminho e nome do componente EntreCenasLanding.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import EntreCenasLanding from './components/EntreCenasLanding'
+import EntreCenasLanding from './components/EntreCenasLanding.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
